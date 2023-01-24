b760d29623b654bb8b1b66db0e50fa5b0963ff56
tim5046 <tim5046@gmail.com>
2023-01-24 15:36:05 -0600

    Add chakra provider

--- src/sxastarter/src/pages/_app.tsx
+++ src/sxastarter/src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import type { AppProps } from 'next/app';
-import { I18nProvider } from 'next-localization';
-import { SitecorePageProps } from 'lib/page-props';
+import type { AppProps } from "next/app";
+import { I18nProvider } from "next-localization";
+import { SitecorePageProps } from "lib/page-props";
+import { ChakraBaseProvider } from "@chakra-ui/react";
 
-import 'assets/main.scss';
+import "assets/main.scss";
 
-function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
+function App({
+  Component,
+  pageProps,
+}: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
 
   return (
@@ -12,7 +16,9 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
     // Note Next.js does not (currently) provide anything for translation, only i18n routing.
     // If your app is not multilingual, next-localization and references to it can be removed.
     <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
-      <Component {...rest} />
+      <ChakraBaseProvider>
+        <Component {...rest} />
+      </ChakraBaseProvider>
     </I18nProvider>
   );
 }
